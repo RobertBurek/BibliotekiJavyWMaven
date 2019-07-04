41db9b8f6d9d6b278e5269f9f746aed83e072266
Robert Burek <robertburek@wp.pl>
2019-07-04 15:34:02 +0200

    LEMBOK -  @Getter i @Setter

---
+++ src/main/java/pl/robertburek/App.java
@@ -0,0 +1,24 @@
+package pl.robertburek;
+
+import lombok.AllArgsConstructor;
+
+
+/**
+ * Hello world!
+ */
+@AllArgsConstructor
+public class App {
+    Boolean a;
+    Boolean b;
+    Boolean c;
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello World!");
+        new App(true, true, true);
+
+        ExampleModel exampleModel = new ExampleModel();
+        exampleModel.nazwisko = "Nowak";
+
+    }
+}
---
+++ src/main/java/pl/robertburek/ExampleModel.java
@@ -0,0 +1,24 @@
+package pl.robertburek;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * Created by Robert Burek
+ */
+public class ExampleModel {
+
+    @Getter
+    @Setter
+    private String imie;
+    @Getter
+    @Setter
+    String nazwisko;
+    @Getter
+    @Setter
+    int wiek;
+    @Getter
+    @Setter
+    boolean maWlosy;
+
+}
