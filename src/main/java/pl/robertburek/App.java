912da2e608d3eba3df14bd48cbad9da02f978fc3
Robert Burek <robertburek@wp.pl>
2019-07-07 13:48:20 +0200

    OKHttp - wysyłanie danych na serwer

--- src/main/java/pl/robertburek/App.java
+++ src/main/java/pl/robertburek/App.java
@@ -2,49 +2,38 @@ package pl.robertburek;
 
 import lombok.extern.java.Log;
 import okhttp3.*;
-import org.jetbrains.annotations.NotNull;
-import org.simpleframework.xml.Serializer;
-import org.simpleframework.xml.core.Persister;
-
-import java.io.IOException;
 
 
 /**
- * Hello world!
+ * Created by Robert Burek
  */
 @Log
 public class App {
 
     public static void main(String[] args) throws Exception {
 
-        //Tworzymy klienta http
-        OkHttpClient client = new OkHttpClient();
 
-        //Tworzemy zapytanie http (gdzie są dane?)
-        // generator xml : https://countwordsfree.com/xmlviewer#save
-        Request request = new Request.Builder()
-                .url("https://countwordsfree.com/download/txt/34d6c2f8-a9b3-4b3d-a6c8-3302ebc4a588")
+        //  Wysyłanie danych na serwer
+        System.out.println("--------------------Wysyłanie na serwer----------------------");
+
+        OkHttpClient clientOut = new OkHttpClient();
+
+        RequestBody body = new FormBody.Builder()
+                .add("id", "1")
+                .add("title", "foo")
+                .add("body", "bar")
+                .add("userId", "1")
                 .build();
 
-        //Tworzymy reakcję na pobranie lub nie pobranie danych
-        client.newCall(request).enqueue(new Callback() {
-            @Override
-            public void onFailure(@NotNull Call call, @NotNull IOException e) {
-                log.info(e.toString());
-            }
+        Request requestOut = new Request.Builder()
+                .url("https://jsonplaceholder.typicode.com/posts/1")
+                .headers(new Headers.Builder().add("Content-type", "application/json; charset=UTF-8").build())
+                .method("PUT", body)
+                .build();
 
-            @Override
-            public void onResponse(@NotNull Call call, @NotNull Response response) throws IOException {
-                String xml = response.body().string();
-                Serializer serializer = new Persister();
-                try {
-                    Ubranie noweUbranie = serializer.read(Ubranie.class, xml);
-                    log.info(noweUbranie.toString());
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            }
-        });
+
+        Response responseOut = clientOut.newCall(requestOut).execute();
+        log.info(responseOut.body().string());
 
         log.info("koniec bloku");
     }
